741e5de16a8ba85db194a174a1df813be37318be
daeuneee <95597381+daeuneee@users.noreply.github.com>
2022-01-18 19:00:23 +0900

    Create daeun.cpp

---
+++ level3/220118_87391/daeun.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+long long solution(int n, int m, int x, int y, vector<vector<int>> queries) {
+    long long answer = 0;
+    int sx = x;
+    int ex = x;
+    int sy = y;
+    int ey = y;
+    int dir, dx;
+    // printf("%d %d %d %d \n", sx, ex, sy, ey);
+    // 목적위치에서 반대로 가용 범위를 찾아감
+    for(int i = queries.size() - 1; i >= 0; i--){
+        dir = queries[i][0];
+        dx = queries[i][1];
+        // printf("%d : %d %d\n", i, queries[i][0], queries[i][1]);
+        switch(dir){
+            // 열 다시 증가
+            case 0:
+                if(sx != 0)
+                    sx += dx;
+                    if(sx > m-1)
+                        sx = m-1;
+                ex += dx;
+                if(ex > m-1)
+                    ex = m-1;
+                break;
+            // 열 다시 감소 
+            case 1:
+                sx -= dx;
+                if(sx < 0)
+                    sx = 0;
+                if(ex != m-1)
+                    ex -= dx;
+                    if(ex < 0)
+                        ex = 0;
+                break;
+            // 행 다시 증가
+            case 2:
+                if(sy != 0)
+                    sy += dx;
+                    if(sy > n-1)
+                        sy = n-1;
+                ey += dx;
+                if(ey > n-1)
+                    ey = n-1;
+                break;
+            // 행 다시 감소
+            case 3:
+                sy -= dx;
+                if(sy < 0)
+                    sy = 0;
+                if(ey != n-1)
+                    ey -= dx;
+                    if(ey < 0)
+                        ey = 0;
+                break;
+        }
+        if(sx > m-1 || ex < 0 || sy > n-1 || ey < 0)
+            return 0;
+    }
+    return ((ex - sx + 1) * (ey - sy + 1));
+}
