48f8a8ec0d67fe0e01a9d49b9e4d644a35567c01
zzudy <wngus1100@gmail.com>
2022-01-24 22:46:46 +0900

    add juhyeon.cpp

---
+++ level3/220125_67259/juhyeon.cpp
@@ -0,0 +1,63 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <math.h>
+
+using namespace std;
+
+int M[26][26];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+
+typedef struct Road
+{
+    int r;
+    int c;
+    int dir;
+    int cost;
+};
+
+int solution(vector<vector<int> > board)
+{
+    int ans=2e9;
+    int N = board.size();
+    queue<Road> q;
+    vector<vector<int> > Cost(N, vector<int>(N, 2e9));
+    q.push({0, 0, -1, 0});
+    Cost[0][0] = 0;
+    while (!q.empty())
+    {
+        int x = q.front().r;
+        int y = q.front().c;
+        int dir = q.front().dir;
+        int cost = q.front().cost;
+        q.pop();
+
+        //도착지점일 경우
+        if (x == N - 1 && y == N - 1)
+        {
+            ans = min(ans, Cost[x][y]);
+            continue;
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            int tx = x + dx[i];
+            int ty = y + dy[i];
+            if (tx > N - 1 || ty > N - 1 || tx < 0 || ty < 0)
+                continue;
+            if (board[tx][ty] == 1)
+                continue;
+
+            //방향 , -1이면 처음, dir ==i면 직선 도로
+            if (dir == -1 || dir == i) cost +=100;
+            else cost+=600;
+
+            if(Cost[tx][ty] >= cost){
+                Cost[tx][ty] = cost;
+                q.push({tx,ty,i,cost});
+            }
+
+        }
+    }
+    return ans;
+}
