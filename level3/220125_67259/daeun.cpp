e6be111de444bc8f42a9454fd855fd61fc794cc0
daeuneee <95597381+daeuneee@users.noreply.github.com>
2022-01-25 21:00:38 +0900

    Update daeun.cpp

--- level3/220125_67259/daeun.cpp
+++ level3/220125_67259/daeun.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
+int dy[4] = {0, 1, 0, -1};
+int dx[4] = {1, 0, -1, 0};
 
 int map[25][25];
 int visited[25][25] = {0, };
@@ -19,12 +19,14 @@ struct CAR{
 };
 
 void dfs(int map[][25], CAR car, int cur_cost){
-    printf("%d %d %d %d %d\n", cost, cur_cost, car.y, car.x, car.dir);
+    
+    // if(N == 3)
+    //     printf("%d %d %d %d %d\n", cost, cur_cost, car.y, car.x, car.dir);
     
     if(cur_cost >= cost){
         return;
     }
-    
+      
     if (car.y == N-1 && car.x == N-1){
         cost = cur_cost;
         return;
@@ -34,20 +36,16 @@ void dfs(int map[][25], CAR car, int cur_cost){
         int ny = car.y + dy[i];
         int nx = car.x + dx[i];
         int nd = i;
-        int flag_dir = 0;
         if(ny < 0 || ny >= N || nx < 0 || nx >= N || visited[ny][nx] == 1 || map[ny][nx] == 1)
             continue;
         // printf("aaaaa\n");
+        int next_cost;
         visited[ny][nx] = 1;
-        cur_cost += 100;
+        next_cost = cur_cost + 100;
         if(car.dir != nd && car.dir != -1){
-            cur_cost += 500;
-            flag_dir = 1;
+            next_cost += 500;
         }
-        dfs(map, {ny, nx, nd}, cur_cost);
-        cur_cost -= 100;
-        if(flag_dir)
-            cur_cost -= 500;
+        dfs(map, {ny, nx, nd}, next_cost);
         visited[ny][nx] = 0;
     }
 }
