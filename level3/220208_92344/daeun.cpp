9780b69ce44a4b8e7d5533aba4c83b3754caf705
daeuneee <95597381+daeuneee@users.noreply.github.com>
2022-02-06 23:44:46 +0900

    Update daeun.cpp

--- level3/220208_92344/daeun.cpp
+++ level3/220208_92344/daeun.cpp
@@ -1,4 +1,4 @@
-// 마지막 3개 시간 초과
+// 해설 추가필요
 #include <string>
 #include <vector>
 #include <stdio.h>
@@ -7,30 +7,51 @@ using namespace std;
 
 int solution(vector<vector<int>> board, vector<vector<int>> skill) {
     int answer = 0;
+    // mask 생성
+    int mask[1001][1001] = {0, };
     
     for (int i = 0; i < skill.size(); i++){
-        // 공격, 방어 계산
+        // mask 계산
         int r1, c1, r2, c2;
         r1 = skill[i][1];
         c1 = skill[i][2];
         r2 = skill[i][3];
         c2 = skill[i][4];
-        for (int y = r1; y < r2 + 1; y++){
-            for (int x = c1; x < c2 + 1; x++){
-                if(skill[i][0] == 1){
-                    board[y][x] -= skill[i][5];
-                }
-                else{
-                    board[y][x] += skill[i][5];
-                }
-            }
+        
+        int degree = skill[i][5];
+        if (skill[i][0] == 1)
+            degree = -degree;
+        
+        mask[r1][c1] += degree;
+        mask[r1][c2+1] += -degree;
+        mask[r2+1][c1] += -degree;
+        mask[r2+1][c2+1] += degree;
+    }
+    
+    // 누적 더하기
+    for (int y = 0; y < board.size()+1; y++){
+        for (int x = 0; x < board[0].size(); x++){
+            mask[y][x+1] = mask[y][x] + mask[y][x+1];
+        }
+    }
+
+    for (int x = 0; x < board[0].size()+1; x++){
+        for (int y = 0; y < board.size(); y++){
+            mask[y+1][x] = mask[y][x] + mask[y+1][x];
+        }
+    }
+    
+    // mask와 board 더하기
+    for (int y = 0; y < board.size(); y++){
+        for (int x = 0; x < board[0].size(); x++){
+            mask[y][x] += board[y][x];
         }
     }
     
     // 파괴되지 않은 건물 count
     for (int y = 0; y < board.size(); y++){
         for (int x = 0; x < board[0].size(); x++){
-            if (board[y][x] > 0)
+            if (mask[y][x] > 0)
                 answer++;
         }
     }
