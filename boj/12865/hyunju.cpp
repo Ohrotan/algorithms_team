ce91dd643f3cc8a59d2fd9f3c840d66a4ba9f902
millcham2 <37177508+bsh65792@users.noreply.github.com>
2022-03-21 21:03:21 +0900

    Update hyunju.cpp
    허락맡고 주석 조금 더 달았습니다

--- boj/12865/hyunju.cpp
+++ boj/12865/hyunju.cpp
@@ -9,15 +9,15 @@ int main()
 
 	cin >> N;
 	cin >> W;
-	vector<int> memo(W+1, 0);
+	vector<int> memo(W+1, 0);	//이전 아이템까지 조사했을 때, w무게에서 가질 수 있는 최대 가치를 memo.at(w)로 정의
 	for (int i = 0; i < N; ++i) {
 		int w;
 		int v;
 		cin >> w;
 		cin >> v;
-		vector<int> curMemo = memo;
+		vector<int> curMemo = memo;	//현재 조사중인 아이템의 w무게에서 가질 수 있는 최대 가치를 curMemo.at(w)
 		for (int j = 0; j <= W; ++j) {
-			if (j + w <= W) {
+			if (j + w <= W) {	//현재 최대가치를 조사중인 무게와 현재 조사중인 아이템의 무게 합이 넘어가면 어차피 못담으므로 이전 가치를 그대로 둠
 				if (curMemo[j] + v > memo[j + w]) {
 					memo[j + w] = curMemo[j] + v;
 				}
