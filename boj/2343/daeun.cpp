5eb781426a5298b9066e5829839f83ecdd14354b
daeuneee <95597381+daeuneee@users.noreply.github.com>
2022-08-09 22:06:39 +0900

    Update daeun.cpp

--- boj/2343/daeun.cpp
+++ boj/2343/daeun.cpp
@@ -24,20 +24,16 @@ bool check(int m) {
             cnt++;
             sum = lec[i];
         }
-        else {
-            sum += lec[i];
-        }
+        else sum += lec[i];
         if (cnt > M) return false;
     }
-
     return true;
 }
 
 int solve() {
     int s = 0;
     int e = total;
-    int m;
-    int res;
+    int m, res;
 
     while (s <= e) {
         m = (s + e) / 2;
@@ -45,9 +41,7 @@ int solve() {
             e = m - 1;
             res = m;
         }
-        else {
-            s = m + 1;
-        }
+        else s = m + 1;
     }
     return res;
 }
